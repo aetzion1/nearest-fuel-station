0e3571b3a2162a8b555e3869ab61258bae19b38a
Adam Etzion <adametzion90@gmail.com>
2021-02-26 09:48:15 -0700

    Add farady gem, bundle, and refactor search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,25 @@
 class SearchController < ApplicationController
   def index
+    require 'pry'; binding.pry
+    address = params[:location]
+    @station = nearest_station(address)
+    require 'pry'; binding.pry
+  end
 
   private
 
   def nearest_station(address)
-    conn = Faraday.new(url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest')
-    
-    response = conn.get('/.json') do |req|
-      params = {api_key: ['NREL_API_KEY'],
-      params = {location: address}
+    # refactor into own conn method when in service
+    conn = Faraday.new(
+      url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest',
+      params: { api_key: ENV['NREL_API_KEY'] }
+    )
+
+    response = conn.get('.json') do |req|
+      req.params[:location] = address
     end
     
     data = JSON.parse(response.body, symbolize_names: true)
     @station = json[:fuel_stations][0]
-
-
+  end
 end
