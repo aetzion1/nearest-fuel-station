92266e32d1f441922072fcaf7de1391a584c6fad
Adam Etzion <adametzion90@gmail.com>
2021-02-26 09:19:22 -0700

    Add feature spec to search for nearby station

--- spec/features/user_can_visit_welcome_page_spec.rb
+++ spec/features/user_can_visit_welcome_page_spec.rb
@@ -12,6 +12,14 @@ describe "user can visit the welcome page" do
     visit '/'
     click_button "Find Nearest Station"
     
-    expect(current_path).to eq ``
+    expect(current_path).to eq search_path
+
+    expect(page).to have_content(station.name)
+    expect(page).to have_content(station.address)
+    expect(page).to have_content(station.fuel_type)
+    expect(page).to have_content(station.access_times)
+    expect(page).to have_content(station.distance)
+    expect(page).to have_content(station.travel_time)
+    expect(page).to have_content(station.directions)
   end
 end
